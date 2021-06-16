9caa487f591b7dd5173b595259567d9c1696d018
Praveen <sankar_praveen@hotmail.com>
2021-06-16 20:36:19 +0530

    Initial

---
+++ DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options ): base(options){
+        }
+
+        public DataContext()
+        {
+            
+        }
+
+        public DbSet<Activity> Activities {get; set;}
+    }
+}
